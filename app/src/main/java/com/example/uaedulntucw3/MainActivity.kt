a2cc5891bd2094e777105c6493481b2eed018910
StikNavi <stiknavi@gmail.com>
2024-02-24 13:09:18 +0200

    commit5

--- app/src/main/java/com/example/uaedulntucw3/MainActivity.kt
+++ app/src/main/java/com/example/uaedulntucw3/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.uaedulntucw3
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -13,21 +13,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.uaedulntucw3.ui.theme.Uaedulntucw3Theme
-
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Uaedulntucw3Theme {
-
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
@@ -39,11 +34,14 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+data class ListItemData(val text: String, val imageResource: Int)
+
 @Composable
 fun MyList() {
     val itemList = listOf(
-        Pair("Елемент", R.drawable.study),
-
+        ListItemData("Елемент", R.drawable.study),
+        ListItemData("Елемент", R.drawable.study),
+        ListItemData("Елемент", R.drawable.study)
     )
 
     LazyColumn {
@@ -52,8 +50,6 @@ fun MyList() {
         }
     }
 }
-
-data class ListItemData(val text: String, val imageResource: Int)
 
 @Composable
 fun ListItem(item: ListItemData) {
@@ -84,7 +80,7 @@ fun ImageWithText(imageResource: Int, modifier: Modifier = Modifier) {
         modifier = modifier
     ) {
         val image = painterResource(id = imageResource)
-        androidx.compose.foundation.Image(
+        Image(
             painter = image,
             contentDescription = null,
             modifier = Modifier
